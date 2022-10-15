055cce42209e5cacd6a606873725afca14494cac
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 19:18:19 +0530

    Update

--- lib/Utils/ToDo_Tile.dart
+++ lib/Utils/ToDo_Tile.dart
@@ -5,6 +5,6 @@ class ToDoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text(data),);
+    return Container(child: Text('data'),);
   }
 }
