f094895e37c42aad924013032cae48b9026832e3
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 20:39:54 +0530

    Update

--- lib/Utils/Dialog_Box.dart
+++ lib/Utils/Dialog_Box.dart
@@ -33,7 +33,10 @@ class DialogBox extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                MyButton(text: "Save", onPressed: onSave,),
+                MyButton(
+                  text: "Save",
+                  onPressed: onSave,
+                ),
                 const SizedBox(width: 8),
                 MyButton(
                   text: "Cancel",
