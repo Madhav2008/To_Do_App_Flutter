7fbabaabf35a9b97a1c533ce3cd3c8d4d160399c
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 19:33:51 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ToDoTile(
             taskName: "Task 1",
             taskCompleted: false, 
-            onChanged: (bool? ) {  },
+            onChanged: (bool) {  },
           ),
         ],
       ),
