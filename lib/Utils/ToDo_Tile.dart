57926de3836cd631932c2e807d902e052f4bfd06
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 20:21:28 +0530

    Update

--- lib/Utils/ToDo_Tile.dart
+++ lib/Utils/ToDo_Tile.dart
@@ -11,7 +11,7 @@ class ToDoTile extends StatelessWidget {
 
   final String taskName;
   final bool taskCompleted;
-  Function(bool?)? onChanged;
+  Function(bool?)? onChanged;Function(BuildContext)? deleteFunction;
 
   @override
   Widget build(BuildContext context) {
