a31e1c2ff1d90e001a63a08691193088145a8035
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 19:26:39 +0530

    Update

--- lib/Utils/ToDo_Tile.dart
+++ lib/Utils/ToDo_Tile.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
   const ToDoTile({Key? key}) : super(key: key);
-  
 
+  final
 
   @override
   Widget build(BuildContext context) {
