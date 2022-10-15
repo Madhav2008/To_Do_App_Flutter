dcc17a0aab326a4a536caf1dce961e0216c60969
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 20:22:46 +0530

    Update

--- lib/Utils/ToDo_Tile.dart
+++ lib/Utils/ToDo_Tile.dart
@@ -22,28 +22,30 @@ class ToDoTile extends StatelessWidget {
         right: 25,
         top: 25,
       ),
-      child: Container(
-        padding: const EdgeInsets.all(24.0),
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          children: [
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.black,
-            ),
-            Text(
-              taskName,
-              style: TextStyle(
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
+      child: Slidable(
+        child: Container(
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
               ),
-            ),
-          ],
+              Text(
+                taskName,
+                style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
