f89caacb3237677f457255d1a0bb5f080dfffd21
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 20:10:08 +0530

    Update

--- lib/Utils/ToDo_Tile.dart
+++ lib/Utils/ToDo_Tile.dart
@@ -33,7 +33,11 @@ class ToDoTile extends StatelessWidget {
               onChanged: onChanged,
               activeColor: Colors.black,
             ),
-            Text(taskName,),
+            Text(taskName,style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),),
           ],
         ),
       ),
