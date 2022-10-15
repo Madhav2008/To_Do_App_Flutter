5b4f36a0436941265bd5e12301420649012c3dc0
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 19:13:50 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -19,7 +19,9 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0,
         centerTitle: true,
       ),
-      body: ListView(children: [],),
+      body: ListView(
+        children: [],
+      ),
     );
   }
 }
