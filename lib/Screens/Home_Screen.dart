a2f63e85d667f8e257666054cb0e1d2b5fe664cd
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 20:43:58 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -15,6 +15,30 @@ class _HomeScreenState extends State<HomeScreen> {
   final _myBox = Hive.box('mybox');
   ToDoDataBase db = ToDoDataBase();
 
+  @override
+  void initState() {
+    // if this is the 1st time ever openin the app, then create default data
+    if (_myBox.get("TODOLIST") == null) {
+      db.createInitialData();
+    } else {
+      // there already exists data
+      db.loadData();
+    }
+
+    super.initState();
+  }
+
+  // text controller
+  final _controller = TextEditingController();
+
+  // checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      db.toDoList[index][1] = !db.toDoList[index][1];
+    });
+    db.updateDataBase();
+  }
+
   final _controller = TextEditingController();
 
   List toDoList = [
