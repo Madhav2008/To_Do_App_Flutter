8555161ea2b69523c561858527ccb1025d9a2dae
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 18:59:58 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/Screens/Home_Screen.dart';
 
 void main() {
   runApp(
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
        
         primarySwatch: Colors.blue,
       ),
-      home: const,
+      home: const HomeScreen(),
     );
   }
 }
