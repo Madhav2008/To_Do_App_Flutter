7484402cc13a4378739c92959b282e7e1efc93d8
Madhav2008 <aroramadhav65@gmail.com>
2022-10-15 18:59:53 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
        
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const,
     );
   }
 }
